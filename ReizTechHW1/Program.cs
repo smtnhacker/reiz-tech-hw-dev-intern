54c5c8fc7f6bf6cc3d148684433b8517c26d1833
smtnhacker <themikosurara@gmail.com>
2023-06-30 02:08:19 +0800

    initial commit

---
+++ ReizTechHW1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Take the user's input
+Console.WriteLine("Please input current hour: ");
+float hours = (float) Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please input current minute: ");
+float minutes = (float) Convert.ToInt32(Console.ReadLine());
+
+// Normalize hours
+if (hours == 12)
+{
+    hours = 0;
+}
+
+// Consider the movement of the hour hand
+// due to the movement of the minute hand
+hours += minutes / 60;
+
+// Define constants for clarity
+const float HourRate = 360f / 12f;
+const float MinuteRate = 360f / 60f;
+
+// Compute the angle
+float angle = Math.Abs(HourRate * hours - MinuteRate * minutes);
+
+// Take the lesser angle
+float ans = Math.Min(angle, 360 - angle);
+
+// Finally, print the output
+Console.WriteLine(String.Format("The angle is {0} deg.", ans));
---
+++ ReizTechHW2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class Branch
+{
+    public List<Branch> branches = new List<Branch>();
+
+    public void Add(Branch branch)
+    {
+        branches.Add(branch);
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        List<Branch> branchList = new List<Branch>();
+        for(int i=0; i<10; i++)
+        {
+            branchList.Add(new Branch());
+        }
+
+        Branch root = new Branch();
+
+        // manually add the branches
+        root.Add(branchList[0]);
+        root.Add(branchList[1]);
+        branchList[0].Add(branchList[2]);
+        branchList[1].Add(branchList[3]);
+        branchList[1].Add(branchList[4]);
+        branchList[1].Add(branchList[5]);
+        branchList[3].Add(branchList[6]);
+        branchList[4].Add(branchList[7]);
+        branchList[4].Add(branchList[8]);
+        branchList[7].Add(branchList[9]);
+
+        Console.WriteLine(String.Format("Structure has depth {0}.", CalculateDepth(root)));
+    }
+
+    private static int CalculateDepth(Branch node)
+    {
+        int deepestBranch = 0;
+        foreach(Branch child in node.branches)
+        {
+            deepestBranch = Math.Max(deepestBranch, CalculateDepth(child));
+        }
+        return 1 + deepestBranch;
+    }
+
+}
